6dd4fa54331c547abff4fde8b5ef80ee9195b020
8ug8ear <63198655+kxxdev@users.noreply.github.com>
2025-05-29 21:21:05 +0500

    bug fix

--- src/bot.ts
+++ src/bot.ts
@@ -6,7 +6,11 @@ import { roleComposer } from './commands/role';
 import { clearHistoryComposer } from './commands/clearhistory';
 import { messageHandler } from './handlers/message';
 
-const bot = new Bot<CustomContext>(process.env.TELEGRAM_TOKEN || '');
+if (!process.env.TELEGRAM_TOKEN) {
+  throw new Error('TELEGRAM_TOKEN is not defined in environment variables');
+}
+
+const bot = new Bot<CustomContext>(process.env.TELEGRAM_TOKEN);
 
 bot.use(startComposer);
 bot.use(roleComposer);
