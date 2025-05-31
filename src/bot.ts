b04feef7f94116272848a9c992d1ad3d020f4290
8ug8ear <63198655+kxxdev@users.noreply.github.com>
2025-05-31 11:26:31 +0500

    Refactoring

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,10 @@
-import { Bot } from 'grammy';
+import { Bot, GrammyError, HttpError } from 'grammy';
 import 'dotenv/config';
 import { CustomContext } from './types/CustomContext';
 import { startComposer } from './commands/start';
 import { roleComposer } from './commands/role';
-import { clearHistoryComposer } from './commands/clearhistory';
 import { messageHandler } from './handlers/message';
+import { clearHistoryComposer } from './commands/clearhistory';
 
 if (!process.env.TELEGRAM_TOKEN) {
   throw new Error('TELEGRAM_TOKEN is not defined in environment variables');
@@ -12,10 +12,51 @@ if (!process.env.TELEGRAM_TOKEN) {
 
 const bot = new Bot<CustomContext>(process.env.TELEGRAM_TOKEN);
 
+bot.api.setMyCommands([
+  {
+    command: 'start',
+    description: 'Перезапуск бота',
+  },
+  {
+    command: 'clearhistory',
+    description: 'Очистить историю сообщений текущей роли',
+  },
+  {
+    command: 'assistant',
+    description: 'Роль ассистента',
+  },
+  {
+    command: 'therapist',
+    description: 'Роль психотерапевта',
+  },
+  {
+    command: 'coach',
+    description: 'Роль наставника',
+  },
+  {
+    command: 'planner',
+    description: 'Роль планировщика',
+  },
+]);
+
 bot.use(startComposer);
 bot.use(roleComposer);
 bot.use(clearHistoryComposer);
 bot.use(messageHandler);
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}`);
+  const e = err.error;
+
+  if (e instanceof GrammyError) {
+    console.error('Error in request:', e.description);
+  } else if (e instanceof HttpError) {
+    console.error('Could not contact Telegram:', e);
+  } else {
+    console.error('Unknown error:', e);
+  }
+});
 
 bot.start();
 
