75e05022cb28a0080839bb14a923bc2e3dd56f43
8ug8ear <63198655+kxxdev@users.noreply.github.com>
2025-05-31 12:52:56 +0500

    Adding a user access system

--- src/middlewares/checkAdminAccess.ts
+++ src/middlewares/checkAdminAccess.ts
@@ -1,14 +1,12 @@
 import { Context, NextFunction } from 'grammy';
-import { checkApproved } from '../database/userService';
 
-export default async function checkAccess(
+export default async function checkAdminAccess(
   ctx: Context,
   next: NextFunction
 ): Promise<void> {
   const tg_id = String(ctx.from?.id);
-  const is_approved = checkApproved(tg_id);
 
-  if (!tg_id || (!is_approved && tg_id != '600190229')) {
+  if (!tg_id || tg_id != '600190229') {
     await ctx.reply('Отказано в доступе.');
     return;
   } else {
