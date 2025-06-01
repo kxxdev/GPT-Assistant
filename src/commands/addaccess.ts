807dd3214f86e72ecff07eda32a7e27d96fec226
8ug8ear <63198655+kxxdev@users.noreply.github.com>
2025-06-01 15:33:52 +0500

    Update user access system

---
+++ src/commands/addaccess.ts
@@ -0,0 +1,23 @@
+import { Composer } from 'grammy';
+import { CustomContext } from '../types/CustomContext';
+import { createUser, giveUserAccess } from '../database/userService';
+import checkAdminAccess from '../middlewares/checkAdminAccess';
+
+const addAccess = new Composer<CustomContext>();
+
+addAccess.command('addaccess', async (ctx) => {
+  if (!ctx.match) {
+    return await ctx.reply('Вы не указали айди пользователя в команде');
+  }
+  const tg_id = String(ctx.match);
+
+  createUser(tg_id);
+  giveUserAccess(tg_id);
+
+  await ctx.reply(
+    `Доступ пользователю с id <code>${tg_id}</code> предоставлен.`,
+    { parse_mode: 'HTML' }
+  );
+});
+
+export { addAccess };
